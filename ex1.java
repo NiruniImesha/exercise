02a58a1868ed12c073f4b5b618115fe8b647685c
Niruni Imesha <88593456+NiruniImesha@users.noreply.github.com>
2021-10-02 02:43:31 +0530

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
 public class test {
 	public static void main(String a[]) {
-		int max=0
+		int max=0;
 		
 	        int[] no= {0,0,0,0};
-		Scaner scanner=new Scanner(System.in); 
-		Sistem.out.print("enter 1st number ");
+		Scanner scanner=new Scanner(System.in); 
+		System.out.print("enter 1st number ");
 		no[0]=scanner.nextInt();
-		System.out.print("enter 2st number ")
+		System.out.print("enter 2nd number ");
 		no[1]=scanner.nextInt();
-		System out.print("enter 3st number ");
+		System.out.print("enter 3rd number ");
 		no[2]=scanner.nextInt();
-		for(int i=0;i<3;++l)
+
+		
+		for(int i=0;i<3;++i)
 		{
 			if(max<=no[i])
 			{
-				mex=no[i];	
+				max=no[i];	
 			}
 		}
 		int min=max;
@@ -23,11 +25,11 @@ public class test {
 		{
 			if(min>=no[i])
 			{
-				min=no[i]
+				min=no[i];
 			}
 		}
 		System.out.println("largest number is "+max);
-		System.out.println("smallest number is "+max);
+		System.out.println("smallest number is "+min);
 		for(int i=0;i<3;++i) {
 			System.out.print(no[i]);
 			System.out.print(" ");
